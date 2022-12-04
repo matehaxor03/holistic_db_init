674045d2b880853c8eb20b29472b32af3026cd8c
matehaxor03 <matehaxor03>
2022-12-05 09:22:04 +1100

    fix field convention

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
+	"bufio"
+	"io/ioutil"
 	class "github.com/matehaxor03/holistic_db_client/class"
 )
 
@@ -20,22 +23,22 @@ func main() {
 func InitDB() []error {
 	var errors []error
 
-	db_hostname, db_port_number, _, root_db_username, root_db_password, root_details_errors := class.GetCredentialDetails("root")
+	db_hostname, db_port_number, _, root_db_username, root_db_password, root_details_errors := getCredentialDetails("root")
 	if root_details_errors != nil {
 		errors = append(errors, root_details_errors...)
 	}
 	
-	_, _, db_name, migration_db_username, migration_db_password, migration_details_errors := class.GetCredentialDetails("holistic_migration")
+	_, _, db_name, migration_db_username, migration_db_password, migration_details_errors := getCredentialDetails("holistic_migration")
 	if migration_details_errors != nil {
 		errors = append(errors, migration_details_errors...)
 	}
 
-	_, _, _, write_db_username, write_db_password, write_details_errors := class.GetCredentialDetails("holistic_write")
+	_, _, _, write_db_username, write_db_password, write_details_errors := getCredentialDetails("holistic_write")
 	if write_details_errors != nil {
 		errors = append(errors, write_details_errors...)
 	}
 
-	_, _, _, read_db_username, read_db_password, read_details_errors := class.GetCredentialDetails("holistic_read")
+	_, _, _, read_db_username, read_db_password, read_details_errors := getCredentialDetails("holistic_read")
 	if read_details_errors != nil {
 		errors = append(errors, read_details_errors...)
 	}
@@ -74,22 +77,16 @@ func InitDB() []error {
 		return errors
 	}
 
-	host, host_errors := class.NewHost(&db_hostname, &db_port_number)
-	client, client_errors := class.NewClient(host, &root_db_username, nil)
-
-	encoding := "utf8mb4"
-	collate := "utf8mb4_0900_ai_ci"
-	database_create_options := class.NewDatabaseCreateOptions(&encoding, &collate)
-	
-	options := make(map[string]map[string][][]string)
-	logic_options := make(map[string][][]string)
-	logic_options["CREATE"] = append(logic_options["CREATE"], class.GET_LOGIC_STATEMENT_IF_NOT_EXISTS())
-	options[class.GET_LOGIC_STATEMENT_FIELD_NAME()] = logic_options
-
-	if host_errors != nil {
-		errors = append(errors, host_errors...)
+	client_manager, client_manager_errors := class.NewClientManager()
+	if client_manager_errors != nil {
+		errors = append(errors, client_manager_errors...)
 	}
 
+	if len(errors) > 0 {
+		return errors
+	}
+
+	client, client_errors := client_manager.GetClient("holistic_db_config:" + db_hostname + ":" + db_port_number + ":" + db_name + ":" + root_db_username)
 	if client_errors != nil {
 		errors = append(errors, client_errors...)
 	}
@@ -104,8 +101,12 @@ func InitDB() []error {
 	}
 	
 	if !(*database_exists) {
+
+		character_set := class.GET_CHARACTER_SET_UTF8MB4()
+		collate := class.GET_COLLATE_UTF8MB4_0900_AI_CI()
+
 		fmt.Println("creating database...")
-		_, database_creation_errs := client.CreateDatabase(db_name, database_create_options)
+		_, database_creation_errs := client.CreateDatabase(db_name, &character_set, &collate)
 		if database_creation_errs != nil {
 			errors = append(errors, database_creation_errs...)		
 			return errors
@@ -114,8 +115,15 @@ func InitDB() []error {
 		fmt.Println("(skip) database already exists...")
 	}
 
-	database, use_database_errors := client.UseDatabaseByName(db_name)
+	database, database_errors := client.GetDatabase()
+	if database_errors != nil {
+		fmt.Println("get database errors ...")
+		return database_errors
+	}
+
+	use_database_errors := client.UseDatabase(database)
 	if use_database_errors != nil {
+		fmt.Println("use database errors ...")
 		return use_database_errors
 	}
 
@@ -124,11 +132,13 @@ func InitDB() []error {
 
 	migration_user_exists, migration_user_exists_errors := client.UserExists(migration_db_username)
 	if migration_user_exists_errors != nil {
+		fmt.Println("migration user exists errors ...")
 		return migration_user_exists_errors
 	}
+
 	if !(*migration_user_exists) {
 		fmt.Println("creating migration database user...")
-		migration_db_user, create_migration_user_errs := client.CreateUser(&migration_db_username, &migration_db_password, &db_hostname)
+		migration_db_user, create_migration_user_errs := client.CreateUser(migration_db_username, migration_db_password, db_hostname)
 		if create_migration_user_errs != nil {
 			return create_migration_user_errs
 		} else {
@@ -141,11 +151,12 @@ func InitDB() []error {
 	} else {
 		fmt.Println("(skip) migration database user already exists...")
 	}
+	
 	migration_db_user, migration_db_user_errors := client.GetUser(migration_db_username)
 	if migration_db_user_errors != nil {
+		fmt.Println("get migration user exists errors ...")
 		return migration_db_user_errors
 	}
-	
 
 	fmt.Println("granting permissions to migration database user...")
 	_, grant_migration_db_user_errors := client.Grant(migration_db_user, "ALL", &database_filter, &table_filter)
@@ -159,7 +170,7 @@ func InitDB() []error {
 	}
 	if !(*write_user_exists) {
 		fmt.Println("creating write database user...")
-		write_db_user, create_write_user_errs := client.CreateUser(&write_db_username, &write_db_password, &db_hostname)
+		write_db_user, create_write_user_errs := client.CreateUser(write_db_username, write_db_password, db_hostname)
 		if create_write_user_errs != nil {
 			return create_write_user_errs
 		} else {
@@ -194,7 +205,7 @@ func InitDB() []error {
 	}
 	if !(*read_user_exists) {
 		fmt.Println("creating read database user...")
-		read_db_user, create_read_user_errs := client.CreateUser(&read_db_username, &read_db_password, &db_hostname)
+		read_db_user, create_read_user_errs := client.CreateUser(read_db_username, read_db_password, db_hostname)
 		if create_read_user_errs != nil {
 			return create_read_user_errs
 		} else {
@@ -222,13 +233,6 @@ func InitDB() []error {
 	if use_database_username_errors != nil {
 		return use_database_username_errors
 	}
-
-
-
-	database_migration_schema := class.Map {"database_migration_id": class.Map {"type": "uint64", "auto_increment": true, "primary_key": true},
-		"current": class.Map {"type": "*int64", "default": -1},
-		"desired": class.Map {"type": "*int64", "default": 0},
-	}
 	
 
 	data_migration_table_exists, data_migration_table_exists_errors := database.TableExists("DatabaseMigration")
@@ -237,6 +241,13 @@ func InitDB() []error {
 	}
 
 	if !(*data_migration_table_exists) {
+
+		database_migration_schema := class.Map {"database_migration_id": class.Map {"type": "uint64", "auto_increment": true, "primary_key": true},
+			"title": class.Map {"type": "string", "default": "", "max_length":255 },
+			"current": class.Map {"type": "int64", "default": -1},
+			"desired": class.Map {"type": "int64", "default": 0},
+		}
+
 		fmt.Println("creating table database migration...")
 		_, create_table_errors := database.CreateTable("DatabaseMigration", database_migration_schema)
 		if create_table_errors != nil {
@@ -263,7 +274,7 @@ func InitDB() []error {
 	}
 
 	fmt.Println("creating database migration record...")
-	inserted_record, inserted_record_errors := data_migration_table.CreateRecord(class.Map{})
+	inserted_record, inserted_record_errors := data_migration_table.CreateRecord(class.Map{"title":"config"})
 	if inserted_record_errors != nil {
 		return inserted_record_errors
 	}
@@ -275,4 +286,83 @@ func InitDB() []error {
 
 	fmt.Println(fmt.Sprintf("created database migration record with primary key: %s", strconv.FormatUint(*inserted_record_value, 10)))
 	return nil
+}
+
+
+func getCredentialDetails(label string) (string, string, string, string, string, []error) {
+	var errors []error
+
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		errors = append(errors, err)
+		return "", "", "", "", "", errors
+	}
+
+	filename := ""
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		currentFileName := file.Name()
+
+		if !strings.HasPrefix(currentFileName, "holistic_db_config:") {
+			continue
+		}
+
+		if !strings.HasSuffix(currentFileName, label+".config") {
+			continue
+		}
+		filename = currentFileName
+	}
+
+	if filename == "" {
+		errors = append(errors, fmt.Errorf("database config for %s not found filename is empty: holistic_db_config|{database_ip_address}|{database_port_number}|{database_name}|{database_username}.config e.g holistic_db_config|127.0.0.1|3306|holistic|root.config", label))
+		return "", "", "", "", "", errors
+	}
+
+	parts := strings.Split(filename, ":")
+	if len(parts) != 5 {
+		errors = append(errors, fmt.Errorf("database config for %s not found filename is in wrong format and had parts: %s holistic_db_config|{database_ip_address}|{database_port_number}|{database_name}|{database_username}.config e.g holistic_db_config|127.0.0.1|3306|holistic|root.config", label, parts))
+		return "", "", "", "", "", errors
+	}
+
+	ip_address := parts[1]
+	port_number := parts[2]
+	database_name := parts[3]
+	username := parts[4]
+	password := ""
+	username = ""
+
+	file, err_file := os.Open(filename)
+	if err_file != nil {
+		errors = append(errors, err_file)
+		return "", "", "", "", "", errors
+	}
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		currentText := scanner.Text()
+		if strings.HasPrefix(currentText, "password=") {
+			password = currentText[9:len(currentText)]
+		}
+		if strings.HasPrefix(currentText, "user=") {
+			username = currentText[5:len(currentText)]
+		}
+	}
+	if file_errs := scanner.Err(); err != nil {
+		errors = append(errors, file_errs)
+	}
+	if password == "" {
+		errors = append(errors, fmt.Errorf("password not found for file: %s", filename))
+	}
+	if username == "" {
+		errors = append(errors, fmt.Errorf("user not found for file: %s", filename))
+	}
+	if len(errors) > 0 {
+		return "", "", "", "", "", errors
+	}
+
+	return ip_address, port_number, database_name, username, password, errors
 }
